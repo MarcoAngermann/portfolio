415ce679a8ae45a4a04c17e3523e4d574eaf4ad8
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-09-15 12:35:13 +0200

    hover link effect

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { WelcomeAreaComponent } from "../welcome-area/welcome-area.component";
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { CommonModule } from '@angular/common';
+import { SkillSectionComponent } from '../skill-section/skill-section.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,AboutMeComponent,WelcomeAreaComponent],
+  imports: [CommonModule,AboutMeComponent,WelcomeAreaComponent,SkillSectionComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
