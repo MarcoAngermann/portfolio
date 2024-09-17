f4581689871c937dd8825e0c220545c52ff05fb0
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-09-17 09:49:18 +0200

    featured project

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,11 +3,12 @@ import { WelcomeAreaComponent } from "../welcome-area/welcome-area.component";
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { CommonModule } from '@angular/common';
 import { SkillSectionComponent } from '../skill-section/skill-section.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,AboutMeComponent,WelcomeAreaComponent,SkillSectionComponent],
+  imports: [CommonModule,AboutMeComponent,WelcomeAreaComponent,SkillSectionComponent,ProjectsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+  hoveredImageUrl: string | null = null;
+
+  setHoveredProject(imageUrl: string | null): void {
+    this.hoveredImageUrl = imageUrl;
+  }
+}
