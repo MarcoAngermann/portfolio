d242623b4e32fc1ce72cb72ed914dcb4ca98897a
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-09-19 18:53:47 +0200

    update responsive

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,10 +7,11 @@ import { ProjectsComponent } from '../projects/projects.component';
 import { ReferencesComponent } from '../references/references.component';
 import { ContactSectionComponent } from '../contact-section/contact-section.component';
 
+
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,AboutMeComponent,WelcomeAreaComponent,SkillSectionComponent,ProjectsComponent,ReferencesComponent,ContactSectionComponent],
+  imports: [CommonModule, AboutMeComponent, WelcomeAreaComponent, SkillSectionComponent, ProjectsComponent, ReferencesComponent, ContactSectionComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
