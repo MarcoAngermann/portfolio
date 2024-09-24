385c84cded84ce1d3081b9a87646a5abde9d6dad
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-09-24 12:04:01 +0200

    project info card

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,17 +6,69 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
+  projects = [
+    {
+      title: 'Join',
+      subtitle: 'Task Management Tool',
+      description: 'A collaboration platform to manage tasks.',
+      technologies: [
+        { iconUrl: '../../assets/img/javascriptgreen.svg' },
+        { iconUrl: '../../assets/img/htmlgreen.svg' },
+        { iconUrl: '../../assets/img/cssgreen.svg' },
+        { iconUrl: '../../assets/img/firebasegreen.svg' }
+      ],
+      imageUrl: '../../assets/img/join-hover.svg',
+      infoUrl: '../../assets/img/joinbig.png'
+    },
+    {
+      title: 'El Pollo Loco',
+      subtitle: '2D Game',
+      description: 'A funny 2D platformer game.',
+      technologies: [
+        { iconUrl: '../../assets/img/htmlgreen.svg' },
+        { iconUrl: '../../assets/img/cssgreen.svg' },
+        { iconUrl: '../../assets/img/javascriptgreen.svg' }
+      ],
+      imageUrl: '../../assets/img/pollo-hover.svg',
+      infoUrl: '../../assets/img/elpollobig.png'
+    },
+    {
+      title: 'DA Bubble',
+      subtitle: 'Social Media App',
+      description: 'A social media platform for sharing content.',
+      technologies: [
+        { iconUrl: '../../assets/img/angulargreen.svg' },
+        { iconUrl: '../../assets/img/typescriptgreen.svg' },
+        { iconUrl: '../../assets/img/htmlgreen.svg' },
+        { iconUrl: '../../assets/img/cssgreen.svg' },
+        { iconUrl: '../../assets/img/firebasegreen.svg' }
+      ],
+      imageUrl: '../../assets/img/bubble-hover.svg',
+      infoUrl: '../../assets/img/dabubblebig.png'
+    }
+  ];
+
+  infoImageUrl: string | null = null;
   hoveredImageUrl: string | null = null;
+  showProjectInfo: boolean = false;
+  selectedProject: any = null;
 
   setHoveredProject(imageUrl: string | null): void {
     this.hoveredImageUrl = imageUrl;
+   
   }
 
-  projektInfo() {
-    console.log('Projekt Info');
+  infoImage(infoUrl: string | null): void {
+    this.infoImageUrl = infoUrl;
+  }
+
+  selectProject(project: any): void {
+    this.selectedProject = project;
+    this.showProjectInfo = true;
   }
 }
+
 
