098aa1f438698a25326d6005da004a44bfb4f31b
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-09-29 13:07:43 +0200

    routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PolicyComponent } from './policy/policy.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
+    {path: 'imprint', component: ImprintComponent},
+    {path: 'policy', component: PolicyComponent}
 ];
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ImprintComponent } from '../../imprint/imprint.component';
+import { PolicyComponent } from '../../policy/policy.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ImprintComponent,PolicyComponent,RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
