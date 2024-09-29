fe5f25d11637f643fa2875f30111098ea96251a8
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-09-29 13:47:40 +0200

    scrollToTop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PolicyComponent } from './policy/policy.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'imprint', component: ImprintComponent},
     {path: 'policy', component: PolicyComponent}
 ];
+
