e57230dedc776d3d6a45ff916b73e0881e683828
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-09-29 10:55:03 +0200

    imprint & policy

---
+++ src/app/imprint/imprint.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [CommonModule,TranslateModule],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintComponent {
+  constructor(public translateService: TranslateService) {}
+}
